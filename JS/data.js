fad0536c753b15e25eda3673e0faf4de61c8a776
EmanuelR0sa <120272063+EmanuelR0sa@users.noreply.github.com>
2023-04-20 22:19:49 +0100

    Add files via upload

---
+++ JS/data.js
@@ -0,0 +1,29 @@
+let shopItemData = [{
+    id: "laptop",
+    name: "Dell XPS 13 Plus Laptop",
+    price: 1999,
+    desc: "Processor-12ᵗʰ Gen Intel® Core™ i5-1240P, OS-Windows 11 Home, Graphics-Intel® Iris® Xe Graphics, Memory-8 GB, Storage-512 GB SSD, Display-13.4-in. display Full HD (1920X1200).",
+    img: "./img/DELLXPS1302.png"
+},
+{
+    id: "Camera",
+    name: "DJI Mavic 3 Classic",
+    price: 1500,
+    desc: "Takeoff Weight-895 g, Dimensions Folded-221×96.3×90.3 mm (L×W×H),Dimensions Unfolded-47.5×283×107.7 mm (L×W×H), Max Takeoff Altitude-6000 m, Max Flight Time-46 minutes, Camera-20 MP.",
+    img: "./img/DJIdrone01.png"
+
+},
+{
+    id: "MxMini",
+    name: "Logitech MX Keys Keyboard",
+    price: 119,
+    desc: "Dimensions-131.95x295.99x20.97mm, Connectivity-Bluetooth, Colour-Graphite,Operating System-Windows/macOS,Special feature-Ergonomic",
+    img: "./img/KEYSMINI01.png"
+},
+{
+    id: "Headset",
+    name: "Meta Quest 2 Headset",
+    price: 455,
+    desc: "Batteries-4 AA batteries, Product Dimensions-26 x 18.7 x 12.6 cm,Colour-White, Storage capacity-128 GB, Connectivity-Wireless headset.",
+    img: "./img/VRMeta201.png"
+}];

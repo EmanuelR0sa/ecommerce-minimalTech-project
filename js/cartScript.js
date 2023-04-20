43d3786ad30e8b09a06ec2721633dcdeb558c250
EmanuelR0sa <120272063+EmanuelR0sa@users.noreply.github.com>
2023-04-20 22:12:48 +0100

    Delete cartScript.js

--- js/cartScript.js
+++
@@ -1,7 +0,0 @@
-const laptopQuantity = document.getElementById('laptopQuantity');
-const laptopCost = document.getElementById('laptopCost');
-
-let laptopsPurchased = parseInt(localStorage.getItem('cartNumbers'));
-let laptopsTotal = parseInt(localStorage.getItem('TotalCost'));
-laptopQuantity.innerText = laptopsPurchased;
-laptopCost.innerText =laptopsTotal;
